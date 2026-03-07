3a8224064d87b83609c2a3bcc0cf76afac7a0171
Malik Ali <alicoder592@gmail.com>
2026-03-07 21:46:21 +0500

    place created for the dashboard priiview

---
+++ app/(Home Page)/Feature Cards/dashbord.tsx
@@ -0,0 +1,21 @@
+export default function DashboardPreview() {
+    return (
+        <section className="py-24 px-6">
+
+  <div className="max-w-6xl mx-auto">
+
+    <div className="rounded-2xl overflow-hidden border border-slate-200 dark:border-slate-800 shadow-2xl">
+
+      <img
+        src="/dashboard-preview.png"
+        alt="dashboard"
+        className="w-full"
+      />
+
+    </div>
+
+  </div>
+
+</section>
+    );
+}
